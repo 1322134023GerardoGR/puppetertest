a61ca5105cf921bd27b625aa766ef6a457667956
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2024-08-05 15:49:51 -0600

    feat(Pokemon): add save of single table

--- src/scraping/scraping.service.ts
+++ src/scraping/scraping.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as puppeteer from 'puppeteer';
 import { PokemonEntity } from "./entity/pokemon.entity";
+
 @Injectable()
 export class ScrapingService {
   constructor(
@@ -13,37 +14,44 @@ export class ScrapingService {
   async scrapeWebsite(url: string): Promise<PokemonEntity[]> {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.goto(url);
 
-    try {
-      await page.goto(url);
-      const data = await page.evaluate(() => {
-        const rows = Array.from(document.querySelectorAll('table tbody tr'));
-        return rows.map(row => {
-          const columns = row.querySelectorAll('td');
-          return {
-            number: columns[0]?.textContent?.trim(),
-            name: columns[1]?.textContent?.trim(),
-            type1: columns[2]?.textContent?.trim(),
-            type2: columns[3]?.textContent?.trim() || null,
-          };
-        });
+    const data = await page.evaluate(() => {
+      const rows = Array.from(document.querySelectorAll('table tbody tr'));
+      return rows.map(row => {
+        const columns = row.querySelectorAll('td');
+
+        const number = columns[0]?.textContent?.trim();
+        const name = columns[2]?.querySelector('a')?.textContent?.trim();
+
+        // Si no se encuentra el type1, asignar un valor predeterminado
+        const type1Element = columns[3]?.querySelectorAll('a')[1];
+        const type1 = type1Element ? type1Element.textContent?.trim() : 'Unknown';
+
+        // Si no se encuentra el type2, dejarlo como null
+        const type2Element = columns[4]?.querySelectorAll('a')[1];
+        const type2 = type2Element ? type2Element.textContent?.trim() : null;
+
+        return {
+          number,
+          name,
+          type1,
+          type2,
+        };
       });
+    });
 
-      const entities = data.map(item => {
-        const entity = new PokemonEntity();
-        entity.number = item.number;
-        entity.name = item.name;
-        entity.type1 = item.type1;
-        entity.type2 = item.type2;
-        return entity;
-      });
+    await browser.close();
 
-      return this.pokemonRepository.save(entities);
-    } catch (error) {
-      console.error('Error scraping website:', error);
-      throw new Error('Failed to scrape website');
-    } finally {
-      await browser.close();
-    }
+    const entities = data.map(item => {
+      const entity = new PokemonEntity();
+      entity.number = item.number;
+      entity.name = item.name;
+      entity.type1 = item.type1;
+      entity.type2 = item.type2;
+      return entity;
+    });
+
+    return this.pokemonRepository.save(entities);
   }
 }
